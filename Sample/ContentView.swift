309d2c801f25ad83cbcc48bb8c82812951d8f53c
Raphael <elrafae16@gmail.com>
2022-01-09 16:19:31 +0900

    Initial Commit

---
+++ Sample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sample
+//
+//  Created by Raphael Amo on 12/29/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sample/SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleApp.swift
+//  Sample
+//
+//  Created by Raphael Amo on 12/29/21.
+//
+
+import SwiftUI
+
+@main
+struct SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
